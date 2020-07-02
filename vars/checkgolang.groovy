afd75a366c3b8e0512d639286c61159de5cbbf0f
juliencoste <64328595+juliencoste@users.noreply.github.com>
2020-07-03 01:50:23 +0200

    Update checkgolang.groovy

--- vars/checkgolang.groovy
+++ vars/checkgolang.groovy
@@ -5,8 +5,3 @@ def call() {
 // Run golint
    sh 'golangci-lint run --enable-all \${WORKSPACE}/...'
     }
-post {
-    always {
-        recordIssues enabledForFailure: true, tool: goLint(), qualityGates: [[threshold: 1, type: 'TOTAL', unstable: true]]
-        }
-    }
